37b2e742e37cd25db8e9047e7b1f1f5cd324040d
Alena Sharai <exultantislupus@gmail.com>
2025-12-04 20:27:18 +0000

    feat: add styles for app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,20 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import BlogPage from './routes/BlogPage.jsx';
 import PostPage from './routes/PostPage.jsx';
 
+import styles from './App.module.css';
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<Navigate replace to='/articles' />} path='/' />
-        <Route element={<BlogPage />} path='/articles' />
-        <Route element={<PostPage />} path='/articles/:id' />
-      </Routes>
+      <div className={styles.app}>
+        <main className={styles.main}>
+          <Routes>
+            <Route element={<Navigate replace to='/articles' />} path='/' />
+            <Route element={<BlogPage />} path='/articles' />
+            <Route element={<PostPage />} path='/articles/:id' />
+          </Routes>
+        </main>
+      </div>
     </BrowserRouter>
   );
 }
