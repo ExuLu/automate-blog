c65b51ccacb831fad99f61a2c64a7ec997d5838f
Alena Sharai <exultantislupus@gmail.com>
2025-12-04 19:41:58 +0000

    refactor: add conditional rendering to detailed article

--- frontend/src/components/ArticleItemDetailed.jsx
+++ frontend/src/components/ArticleItemDetailed.jsx
@@ -5,19 +5,17 @@ const ArticleItemDetailed = () => {
   const { id: postId } = useParams();
   const post = mockPosts.find((post) => post.id === Number(postId));
 
-  if (!post)
-    return (
-      <div>
-        <p>Post not found</p>
-        <Link to='/articles'>Back to Main Page</Link>
-      </div>
-    );
-
   return (
     <div>
-      <h1>{post.name}</h1>
-      <p>{post.text}</p>
-      <p>{post.date}</p>
+      {post ? (
+        <>
+          <h1>{post.name}</h1>
+          <p>{post.text}</p>
+          <p>{post.date}</p>
+        </>
+      ) : (
+        <p>Post not found</p>
+      )}
       <Link to='/articles'>Back to Main Page</Link>
     </div>
   );
