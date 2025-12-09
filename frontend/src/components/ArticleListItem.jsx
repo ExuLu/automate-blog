dc14dd7c51232f6bc9a98f50737daf61e4eb0613
Alena Sharai <exultantislupus@gmail.com>
2025-12-09 14:06:56 +0000

    feat: add hook to the article list component

--- frontend/src/components/ArticleListItem.jsx
+++ frontend/src/components/ArticleListItem.jsx
@@ -2,12 +2,12 @@ import { Link } from 'react-router-dom';
 
 import styles from './ArticleListItem.module.css';
 
-const ArticleListItem = ({ post }) => {
-  const shortText = post.text.substring(0, 99);
+const ArticleListItem = ({ article }) => {
+  const shortText = article.content.substring(0, 99);
 
   return (
-    <Link to={`/articles/${post.id}`} className={styles.card}>
-      <h2 className={styles.title}>{post.name}</h2>
+    <Link to={`/articles/${article.id}`} className={styles.card}>
+      <h2 className={styles.title}>{article.title}</h2>
       <p className={styles.text}>{`${shortText}...`}</p>
       <span className={styles.linkText}>Read more →</span>
     </Link>
